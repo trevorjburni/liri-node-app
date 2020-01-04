d2d4c6544100670d8047a2791779e8b10722516d
Pilot <trevorjburni@gmail.com>
2020-01-04 16:54:52 -0700

    Corrected the formatting of date of events under the concert-this command

--- liri.js
+++ liri.js
@@ -47,26 +47,28 @@ Possible switch cases
 4. do-what-it-says
 else. return with invalid input message, or no input provide message
 */
-switch (String(command)) {
+switchStatement = function (command, search) {
+    switch (String(command)) {
 
-    case "concert-this":
-        concertThis(search);
-        break;
+        case "concert-this":
+            concertThis(search);
+            break;
 
-    case "spotify-this-song":
-        spotifyThis(search);
-        break;
+        case "spotify-this-song":
+            spotifyThis(search);
+            break;
 
-    case "movie-this":
-        movieThis(search);
-        break;
+        case "movie-this":
+            movieThis(search);
+            break;
 
-    case "do-what-it-says":
-        doWhatItSays(search);
-        break;
+        case "do-what-it-says":
+            doWhatItSays(search);
+            break;
 
-    default:
-        console.log("Check your command, it should either be:\n    concert-this <artist/band name here>\n    spotify-this-song <song name here>\n    movie-this <movie name here>\n    do-what-it-says")
+        default:
+            console.log("Check your command, it should either be:\n    concert-this <artist/band name here>\n    spotify-this-song <song name here>\n    movie-this <movie name here>\n    do-what-it-says")
+    }
 }
 
 // Divider for easier readying
@@ -80,7 +82,6 @@ Input: <artist/band name>
 Output: Name of each venue, venue location and date of the event("MM/DD/YYYY")
 */
 function concertThis(search) {
-    console.log(search);
     // Declare and assign artist variable replacing spaces with "+"'s
     var artist = search.replace(/ /g, "+");
 
@@ -99,7 +100,7 @@ function concertThis(search) {
                 resultString += (i + 1) + "." + search + "\n";
                 resultString += "Venue: " + venue + "\n";
                 resultString += "Location: " + location + "\n";
-                resultString += "Date: " + datetime + "\n";
+                resultString += "Date: " + moment(datetime).format('L') + "\n";
                 resultString += divider;
                 // log out the resultString
                 console.log(resultString);
@@ -121,7 +122,35 @@ Input: <song name>
 Output: Artist, song name, preview link to song from Spotify and album the song is from
 */
 function spotifyThis(search) {
-    console.log(search);
+
+    // set default if no search term is givin
+    if (!search) {
+        search = "The Sign Ace of Base";
+    }
+    spotify.search({
+        type: 'track',
+        query: search
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occured: ' + err);
+        }
+        var resultString = "";
+
+        resultString += ("Artist(s): " + data.tracks.items[0].artists[0].name) + "\n";
+        resultString += ("Song: " + data.tracks.items[0].name) + "\n";
+        resultString += ("Preview URL: " + data.tracks.items[0].preview_url) + "\n";
+        resultString += ("Album: " + data.tracks.items[0].album.name) + "\n";
+        resultString += divider;
+
+        fs.appendFile("log.txt", resultString, function (error) {
+            if (error) {
+                console.log(error);
+            }
+            // log out the resultString
+            console.log(resultString);
+        });
+    })
+
 }
 
 // 'movie-this' uses the OMDB API
@@ -130,40 +159,62 @@ Input: <movie name>
 Output: Movie Title, Year of release, IMDB rating, Rotten Tomatoes rating, Country produced, language, plot and actors.
 */
 function movieThis(search) {
+    // set default if no search term is givin
+    if (!search) {
+        search = "Mr. Nobody";
+        getMovieData();
+        console.log("If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/")
+        console.log("It's on Netflix!");
+    } else {
+        getMovieData();
+    }
     // Declare and assign movieName variable replacing spaces with "+"'s
     var movieName = search.replace(/ /g, "+");
 
-    // Create queryUrl
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=" + omdbKey;
-
     // axios call
-    axios.get(queryUrl).then(
-        function (response) {
+    function getMovieData() {
 
-            // build resultString
-            var resultString = "";
-            resultString += "Movie Title: " + response.data.Title + "\n";
-            resultString += "Year Released: " + response.data.Year + "\n";
-            resultString += "IMDB Rating: " + response.data.Ratings[0].Value + "\n";
-            resultString += "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n";
-            resultString += "Produced in: " + response.data.Country + "\n";
-            resultString += "Language: " + response.data.Language + "\n";
-            resultString += "Plot: " + response.data.Plot + "\n";
-            resultString += "Actors: " + response.data.Actors + "\n";
-            resultString += divider;
-            // write to log.txt
-            fs.appendFile("log.txt", resultString, function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                // log out the resultString
-                console.log(resultString);
-            });
-        }
-    )
+        // Create queryUrl
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=" + omdbKey;
+        axios.get(queryUrl).then(
+            function (response) {
+
+                // build resultString
+                var resultString = "";
+                resultString += "Movie Title: " + response.data.Title + "\n";
+                resultString += "Year Released: " + response.data.Year + "\n";
+                resultString += "IMDB Rating: " + response.data.Ratings[0].Value + "\n";
+                // resultString += "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n";
+                resultString += "Produced in: " + response.data.Country + "\n";
+                resultString += "Language: " + response.data.Language + "\n";
+                resultString += "Plot: " + response.data.Plot + "\n";
+                resultString += "Actors: " + response.data.Actors + "\n";
+                resultString += divider;
+                // write to log.txt
+                fs.appendFile("log.txt", resultString, function (error) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    // log out the resultString
+                    console.log(resultString);
+                });
+
+            }
+        )
+    }
 }
-
 // 'do-what-it-says' uses the random.txt file to run one of the above
-function doWhatItSays(search) {
-    console.log(search);
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+
+        command = dataArr[0];
+        search = dataArr[1];
+        switchStatement(command, search);
+    });
 }
+
+switchStatement(command, search);
