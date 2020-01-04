af435587cb98e6cfa0fa0f6e4c17b5d0cab05795
Pilot <trevorjburni@gmail.com>
2020-01-03 19:51:31 -0700

    appropriate require statements have been added, imported node package dependancies.

--- liri.js
+++ liri.js
@@ -1,5 +1,17 @@
+// Require dotenv package
 require("dotenv").config();
 
+// Require spotify api package
+var Spotify = require('node-spotify-api');
+
+// Set the the variable keys to the keys.js file
 var keys = require("./keys.js");
 
+// Assign spotify to the Spotify object, with the key.
 var spotify = new Spotify(keys.spotify);
+
+// Require axios package
+var axios = require("axios");
+
+// Require moment package
+var moment = require("moment");
